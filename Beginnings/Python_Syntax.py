478dc82cb81d1a391731609ee002b558d72303c7
prashanth <tiramareddiprashanth@gmail.com>
2019-02-05 05:38:40 -0500

    Learning Python

---
+++ Beginnings/Python_Syntax.py
@@ -0,0 +1,23 @@
+# converting an integer to a string
+print("I'm" + str(25) + "years old")
+
+# Input the value
+# value = input()
+# print('Hello' +value)
+
+# Python cannot input a string and an integer together, they need to be converted into respective selves before we move on.
+
+# len(), str(), float(), print(), int(), input()
+
+bacon = 20
+print(bacon + 1)
+
+print(str(bacon) + str(1))
+
+print(str(314))
+
+spam = input()
+print("printing and adding string 20");
+print(spam + str(20))
+print("printing converting integer and adding string 20");
+# print(int(spam)+str(20))
